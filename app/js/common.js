ee5dbfa9f4e11af3f7658187331db5e608659e5a
ArtemChubatyi <stgmdota@gmail.com>
2018-11-12 23:50:50 +0200

    updated to verison 1.0.1

--- app/js/common.js
+++ app/js/common.js
@@ -1,40 +1,93 @@
-for (var i=0; i<9; i++) {
-	document.getElementById('game').innerHTML+='<div class="game__block"></div>';
-}
-var turn = 0;
-var crosses = '<i class="fas fa-times"></i>';
-var circles = '<i class="far fa-circle"></i>';
+var ceil            = document.getElementsByClassName('game__block');
+	reset           = document.getElementById('reset');
+	message         = document.getElementById('message');
+	stepCount       = 0;
+    crosses         = '<svg aria-hidden="true" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="svg-inline--fa fa-times fa-w-11 fa-5x"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""></path></svg>';
+    circles         = '<svg aria-hidden="true" data-prefix="far" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-circle fa-w-16 fa-5x"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z" class=""></path></svg>';
+	player          = crosses;
+	winCombinations = [
+		[1,2,3],
+		[4,5,6],
+		[7,8,9],
+		[1,4,7],
+		[2,5,8],
+		[3,6,9],
+		[1,5,9],
+		[7,5,3]
+	];
+	dataX = [];
+	data0 = [];
 
-document.getElementById('game').onclick = function(event) {
-	if (event.target.className == 'game__block') {
-		if (turn % 2 == 0) {
-			event.target.innerHTML += crosses;
+
+for (var i = 0; i < ceil.length; i++) {
+	ceil[i].addEventListener('click', currentStep);
+}
+
+function currentStep() {
+	var num = +this.getAttribute('data-ceil');
+	if(this.innerHTML !== crosses && this.innerHTML !== circles) {
+		this.innerHTML = player;
+		if (player === crosses) {
+			dataX.push(num);
 		}
-		else  {
-			event.target.innerHTML += circles;
+		else {
+			data0.push(num);
 		}
-		turn++;
+		if ((dataX.length > 2 || data0.length > 2 ) && (checkWin(data0, num) || checkWin(dataX, num))
+		) {
+			for (var i = 0; i < ceil.length; i++) {
+				ceil[i].removeEventListener('click', currentStep);
+			}
+			return (message.innerHTML = 'Победил игрок ' + player);
+		} 
+		changePlayer();
+		stepCount++;
+		if (stepCount === 9) {
+			message.innerHTML = 'Ничья';
+		}
 	}
-	checkWinner();
 }
-function checkWinner() {
-	var allBlocks = document.getElementsByClassName('game__block');
-	if (allBlocks[0].innerHTML == crosses && allBlocks[1].innerHTML == crosses && allBlocks[2].innerHTML == crosses) {alert('Победил игрок 1');}
-	if (allBlocks[3].innerHTML == crosses && allBlocks[4].innerHTML == crosses && allBlocks[5].innerHTML == crosses) {alert('Победил игрок 1');}
-	if (allBlocks[6].innerHTML == crosses && allBlocks[7].innerHTML == crosses && allBlocks[8].innerHTML == crosses) {alert('Победил игрок 1');}
-	if (allBlocks[0].innerHTML == crosses && allBlocks[3].innerHTML == crosses && allBlocks[6].innerHTML == crosses) {alert('Победил игрок 1');}
-	if (allBlocks[1].innerHTML == crosses && allBlocks[4].innerHTML == crosses && allBlocks[7].innerHTML == crosses) {alert('Победил игрок 1');}
-	if (allBlocks[2].innerHTML == crosses && allBlocks[5].innerHTML == crosses && allBlocks[8].innerHTML == crosses) {alert('Победил игрок 1');}
-	if (allBlocks[0].innerHTML == crosses && allBlocks[4].innerHTML == crosses && allBlocks[8].innerHTML == crosses) {alert('Победил игрок 1');}
-	if (allBlocks[3].innerHTML == crosses && allBlocks[4].innerHTML == crosses && allBlocks[7].innerHTML == crosses) {alert('Победил игрок 1');}
-	
-	if (allBlocks[0].innerHTML == circles && allBlocks[1].innerHTML == circles && allBlocks[2].innerHTML == circles) {alert('Победил игрок 2');}
-	if (allBlocks[3].innerHTML == circles && allBlocks[4].innerHTML == circles && allBlocks[5].innerHTML == circles) {alert('Победил игрок 2');}
-	if (allBlocks[6].innerHTML == circles && allBlocks[7].innerHTML == circles && allBlocks[8].innerHTML == circles) {alert('Победил игрок 2');}
-	if (allBlocks[0].innerHTML == circles && allBlocks[3].innerHTML == circles && allBlocks[6].innerHTML == circles) {alert('Победил игрок 2');}
-	if (allBlocks[1].innerHTML == circles && allBlocks[4].innerHTML == circles && allBlocks[7].innerHTML == circles) {alert('Победил игрок 2');}
-	if (allBlocks[2].innerHTML == circles && allBlocks[5].innerHTML == circles && allBlocks[8].innerHTML == circles) {alert('Победил игрок 2');}
-	if (allBlocks[0].innerHTML == circles && allBlocks[4].innerHTML == circles && allBlocks[8].innerHTML == circles) {alert('Победил игрок 2');}
-	if (allBlocks[3].innerHTML == circles && allBlocks[4].innerHTML == circles && allBlocks[7].innerHTML == circles) {alert('Победил игрок 2');}
+
+function changePlayer() {
+	if(player === crosses) {
+		player = circles;
+	}
+	else {
+		player = crosses;
+	}
+	message.innerHTML = 'Ходит игрок ' + player;
 }
+
+ reset.addEventListener('click', clear);
+
+ function clear() {
+	for (var i = 0; i < 9; i ++) {
+		ceil[i].innerHTML = '';
+	}
+	dataX = [];
+	data0 = [];
+	player = crosses;
+	message.innerHTML = 'Ходит игрок ' + player;
+	stepCount = 0;
+	for (var i = 0; i < ceil.length; i++) {
+		ceil[i].addEventListener('click', currentStep);
+	}
+ }
+ function checkWin(arr, number) {
+	for(var i = 0, iLen = winCombinations.length; i < iLen; i++ ) {
+		var possiblyWinningArray = winCombinations[i],
+			count = 0;
+		if(possiblyWinningArray.indexOf(number)!== -1) {
+			for(j = 0, jLen = possiblyWinningArray.length; j < jLen; j++) {
+				if ( arr.indexOf(possiblyWinningArray[j]) !== -1) {
+					count++;
+					if (count ===3) {
+						return true;
+					}
+				}
+			}
+			count = 0;
+		}
+	}
+ }
 	
