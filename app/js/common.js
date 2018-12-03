857364612a8ba19e1a221445ac8aac3cc194d2f8
ArtemChubatyi <stgmdota@gmail.com>
2018-12-04 01:19:32 +0200

    updated to v1.4.0

--- app/js/common.js
+++ app/js/common.js
@@ -1,60 +1,93 @@
 var block            		= document.getElementsByClassName('game_block');
-	coefficient 			= +prompt('Enter the number');
+	coefficient				= 3;
 	quantity 				= coefficient * coefficient;
 	reset          		 	= document.getElementById('reset');
 	message         		= document.getElementById('message');
-	// smallGame      			= document.getElementById('small');
 	game  					= document.getElementById('game');
 	stepCount       		= 0;
-    crosses         		= '<svg aria-hidden="true" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="svg-inline--fa fa-times fa-w-11 fa-5x"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""></path></svg>';
-    circles         		= '<svg aria-hidden="true" data-prefix="far" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-circle fa-w-16 fa-5x"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z" class=""></path></svg>';
+    crosses         		= '<svg aria-hidden="true" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="svg-inline--fa fa-times fa-w-11 fa-5x"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""> stub </path></svg>';
+    circles         		= '<svg aria-hidden="true" data-prefix="far" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-circle fa-w-16 fa-5x"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z" class=""> stub </path></svg>';
 	player          		= crosses;
+	stub					= " stub ";
+	path1					= '<path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""> stub </path>';
+	path2					= '<path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z" class=""> stub </path>';
 
 
-game.style.width = (70 * coefficient) + 6 + 'px';
-game.style.height = (70 * coefficient) + 6 + 'px';
 
-for (var c = 0; c < coefficient; c++) {
-	for (var i = 0; i <  coefficient; i++) {
-		game.innerHTML += `<div class="game_block" data-row="${c}"  data-COLUMN="${i}"></div>`;
+
+function build () {
+	game.style.width = (70 * coefficient) + 6 + 'px';
+	game.style.height = (70 * coefficient) + 6 + 'px';
+	for (var c = 0; c < coefficient; c++) {
+		for (var i = 0; i <  coefficient; i++) {
+			game.innerHTML += `<div class="game_block" data-row="${c}"  data-COLUMN="${i}"></div>`;
+		}
 	}
+	function matrixArray(){
+		var arr = new Array();
+		for(var i=0; i<coefficient; i++){
+		  arr[i] = new Array();
+		  for(var j=0; j<coefficient; j++){
+			arr[i][j] = 0;
+		  }
+		}
+		return arr;
+	  }
+	
+	myMatrix = matrixArray();
 }
 
-function matrixArray(){
-	var arr = new Array();
-	for(var i=0; i<coefficient; i++){
-	  arr[i] = new Array();
-	  for(var j=0; j<coefficient; j++){
-		arr[i][j] = 0;
-	  }
+
+
+build();
+
+
+
+function rebuild() {
+	quantity = coefficient * coefficient;
+	game.innerHTML = '';
+	build();
+	doReset();
+}
+
+
+
+function changeGame() {
+	var select          = document.getElementById("select");
+	var selectedOption  = select.options[select.selectedIndex].value;
+	if	(selectedOption == 1) {
+		coefficient = 3;
+		rebuild();
 	}
-	return arr;
-  }
-
-var myMatrix = matrixArray();
-
-for(var i = 0; i < myMatrix.length; i++) {
-	var rowSum = 0;
-	for(var j = 0; j < myMatrix.length ;j++){
-		rowSum += myMatrix[i][j];
+	if (selectedOption == 2) {
+		coefficient = 4;
+		rebuild();
 	}
-  }
-
-// function changeGame() {
-// 	var select          = document.getElementById("select");
-// 	var selectedOption  = select.options[select.selectedIndex].value;
-// 	if	(selectedOption == 1) {
-		
-// 	}
-// 	if (selectedOption == 2) {
-		
-// 	}
-// 	if (selectedOption == 3) {
-		
-// 	}
-// }
-
-// clear();
+	if (selectedOption == 3) {
+		coefficient = 5;
+		rebuild();
+	}
+	if	(selectedOption == 4) {
+		coefficient = 6;
+		rebuild();
+	}
+	if (selectedOption == 5) {
+		coefficient = 7;
+		rebuild();
+	}
+	if (selectedOption == 6) {
+		coefficient = 8;
+		rebuild();
+	}
+	if (selectedOption == 7) {
+		coefficient = 9;
+		rebuild();
+	}
+	if (selectedOption == 8) {
+		coefficient = 10;
+		rebuild();
+	}
+}
 
 
 
@@ -63,9 +96,10 @@ var listener = function(event) {
 	var target = event.target; 
 	var curentRow = target.getAttribute('data-row');
 	var curentCol = target.getAttribute('data-column');
-	console.log(curentCol, curentRow);
 
-	if(target.innerHTML !== crosses && target.innerHTML !== circles) {
+	if(target.innerHTML !== crosses && target.innerHTML !== circles &&
+		target.innerHTML !== stub && target.innerHTML !== path1 && target.innerHTM5L !== path2 && target !== game) {
+
 		target.innerHTML = player;
 		if(player === crosses) {
 			myMatrix[curentRow][curentCol] += 1;
@@ -118,15 +152,17 @@ var listener = function(event) {
 		}
 		changePlayer();
 		stepCount++;
-	} else {
-		alert('это место занято');
-	}
+	} 5
 	if (stepCount === quantity) {
 		message.innerHTML = 'Ничья';
 	}
 };
 
+
+
 game.addEventListener('click', listener, false);
+
+
 
 function changePlayer() {
 	if(player === crosses) {
@@ -139,7 +175,9 @@ function changePlayer() {
 };
 
 
+
 var doReset = function clear() {
+	console.log(quantity, stepCount);	
 	for (var i = 0; i < quantity; i ++) {
 		block[i].innerHTML = '';
 	}
